34cad918ac52264a27990b39416bc1e8049e8907
Tomasz Walotek <tomasz.walotek@gmail.com>
2020-01-26 22:13:05 +0000

    Send readings to MQTT
    Required settings.h:
    
    define WLAN_SSID ""
    define WLAN_PASSWORD ""
    
    define TEMPERATURE_VALUE_TOPIC "uolot/living-room/value/temperature"
    define HUMIDITY_VALUE_TOPIC "uolot/living-room/value/humidity"
    define PRESSURE_VALUE_TOPIC "uoloy/living-room/value/pressure"
    
    define MQTT_USERNAME ""
    define MQTT_PASSWORD ""
    define MQTT_HOST "m21.cloudmqtt.com"
    define MQTT_PORT 18951

--- env-unit/src/main.cpp
+++ env-unit/src/main.cpp
@@ -8,9 +8,20 @@
 #include "DHT12.h"
 #include <Wire.h> //The DHT12 uses I2C comunication.
 #include <Adafruit_BMP280.h>
+#include <WiFi.h>
+#include <MQTT.h>
+#include "settings.h"
+
+
+/* const char *ssid = "3sTK Hyperoptic 1Gb Fibre 2.4Ghz"; */
+/* const char *password = "25sMPZwLGwTRZ"; */
+const char *ssid = WLAN_SSID;
+const char *password = WLAN_PASSWORD;
 
 DHT12 dht12; //Preset scale CELSIUS and ID 0x5c.
 Adafruit_BMP280 bme;
+WiFiClient wifi;
+MQTTClient mqtt;
 
 #define HISTORY 10
 
@@ -21,22 +32,37 @@ float temperature_hist[HISTORY];
 float humidity_hist[HISTORY];
 float pressure_hist[HISTORY];
 
+char buffer[10];
 
-void setup() {
-    M5.begin();
-    M5.Power.begin();
-    Wire.begin();
 
-    M5.Lcd.setBrightness(20);
+void connectWLAN() {
+    Serial.printf("Connecting to %s\r\n", ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(200);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
 
-    Serial.println(F("ENV Unit(DHT12 and BMP280) test..."));
+void connectMQTT() {
+    Serial.print("\nconnecting...");
+    while (!mqtt.connect("m5stack", MQTT_USERNAME, MQTT_PASSWORD)) {
+        Serial.print(".");
+        delay(1000);
+    }
 
+    Serial.println("\nconnected!");
+
+    /* mqtt.subscribe("/hello"); */
+    // mqtt.unsubscribe("/hello");
+}
+
+void populateSensorHistory() {
     while (!bme.begin(0x76)){
       Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-      M5.Lcd.println("Could not find a valid BMP280 sensor, check wiring!");
     }
-    M5.Lcd.clear(BLACK);
-    M5.Lcd.println("ENV Unit test...");
 
     for (int i = 0; i < 10; i++) {
         temperature = dht12.readTemperature();
@@ -49,6 +75,23 @@ void setup() {
 
         delay(50);
     }
+}
+
+void setup() {
+    M5.begin();
+    M5.Power.begin();
+    Wire.begin();
+    WiFi.begin(ssid, password);
+    mqtt.begin(MQTT_HOST, MQTT_PORT, wifi);
+
+    Serial.println("Hello.");
+
+    connectWLAN();
+    connectMQTT();
+    populateSensorHistory();
+
+    M5.Lcd.setBrightness(20);
+    M5.Lcd.clear();
 }
 
 void loop() {
@@ -71,7 +114,7 @@ void loop() {
         pressure_sum += pressure_hist[i];
         pressure_hist[i-1] = pressure_hist[i];
 
-        Serial.printf("[%d]: T: %f, H: %f, P: %f\r\n", i, temperature_hist[i-1], humidity_hist[i-1], pressure_hist[i-1]);
+        /* Serial.printf("[%d]: T: %f, H: %f, P: %f\r\n", i, temperature_hist[i-1], humidity_hist[i-1], pressure_hist[i-1]); */
     }
 
     temperature_sum += temperature;
@@ -92,6 +135,17 @@ void loop() {
     M5.Lcd.setTextColor(WHITE, BLACK);
     M5.Lcd.setTextSize(4);
     M5.Lcd.printf("T: %2.1fC  \r\nH: %2.0f%%  \r\nP: %2.0fhPa  \r\n", temperature, humidity, pressure);
+
+    if (!mqtt.connected()) {
+        connectWLAN();
+        connectMQTT();
+    }
+    sprintf(buffer, "%f", temperature);
+    mqtt.publish(TEMPERATURE_VALUE_TOPIC, buffer);
+    sprintf(buffer, "%f", humidity);
+    mqtt.publish(HUMIDITY_VALUE_TOPIC, buffer);
+    sprintf(buffer, "%f", pressure);
+    mqtt.publish(PRESSURE_VALUE_TOPIC, buffer);
 
     delay(10000);
 }
